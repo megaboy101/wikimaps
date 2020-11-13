de10b0d21a63e76eceb9537114a22ba7167088f0
Josh Ryals <joshryals3@gmail.com>
2020-11-13 11:36:32 -0500

    add title and path endpoints to api.py

--- api/api.py
+++ api/api.py
@@ -1,13 +1,28 @@
 import time
-from flask import Flask
+from flask import Flask, request
+#from flask_restful import Api, resource
 from wikimap import Wikimap
 
 app = Flask(__name__)
 
-@app.route('/path')
+@app.route('/path', methods=['GET'])
+#handle get request for path between two ID's
 def path():
     wikimap = Wikimap('c:\\path\\to\\data.sqlite')
-    src_id = 12345
-    dest_id = 54321
+    src_id = request.args.get('src-id')
+    dest_id = request.args.get('dest-id')
     path = wikimap.determine_path(src_id, dest_id)
+    #path = [src_id, 22222, 33333, 44444, 55555, 66666, dest_id]
     return { 'path': path }
+
+@app.route('/title', methods=['GET'])
+#handle get request for page given page title
+def title():
+    wikimap = Wikimap('c:\\path\\to\\data.sqlite')
+    title = request.args.get('title')
+    page = wikimap.get_page(title)
+    #page = (50000, title)
+    return { 'page': page }
+
+if __name__ == "__main__":
+    app.run(debug=True)
