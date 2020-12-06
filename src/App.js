e184152aca87779c3bb9e0b57ee528120e01b85d
Wuseok Jung <wuseokjung@Wuseoks-MacBook-Pro.local>
2020-12-06 13:10:49 -0500

    UI Design

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
 import { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import FloatingLabelInput from 'react-floating-label-input';
 import './App.css';
+import wikiMapLogo from './WikiMap.png';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+  const [source, setSource] = useState("");
+  const [dest, setDest] = useState("");
 
-  useEffect(() => {
-    console.log(currentTime)
-  }, [currentTime]);
+
+  const handleChangeSrc = (e) => {
+    setSource(e.target.value);
+  }
+  const handleChangeDest = (e) => {
+    setDest(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    //pass source and dest into api
+      e.preventDefault();
+      //call api function
+      fetch('/movies.json')
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+        });
+      // console.log(source);
+      // console.log(dest);
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <a href="#"><img src={wikiMapLogo} className="logoSize"/></a>
+        <p>Finding links for you in the Wiki world.</p>
       </header>
+      <div className="container">
+        <div className="input-field">
+            <FloatingLabelInput className="input" label="Source" id="source" onChange={handleChangeSrc} />
+        </div>
+        <div className="input-field">
+            <FloatingLabelInput className="input" label="Destination" id="destination" onChange={handleChangeDest} />
+            <button className="Button-style" onClick={handleSubmit}>Find</button>
+        </div>
+      </div>           
     </div>
   );
 }
